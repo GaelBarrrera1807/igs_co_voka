9f4e93c7f3aef2417040653a9072af3379f95c9c
rramirez-git <git@rramirez.com>
2024-12-11 11:15:46 -0600

    Adding ramdom prods example

---
+++ producto/datamigration/zza0002_examples_random_prods.py
@@ -0,0 +1,32 @@
+import random as rn
+
+from catalogo.models import CategoriaProducto
+from catalogo.models import TipoCampo
+from catalogo.models import TipoColor
+from catalogo.models import TipoMaterial
+from catalogo.models import TipoParte
+from producto.models import CampoParteProducto
+from producto.models import ParteProducto
+from producto.models import Producto
+
+
+def add_parte(prod):
+    parte = ParteProducto.objects.create(
+        nombre="Notas y Aclaraciones", producto=prod, posicion=1,
+        tipo_de_parte=TipoParte.objects.get(tipo_de_parte="Acabado"))
+    for idx, campo in enumerate(["Largo", "Ancho", "Alto", "Notas y Comentarios", ]):
+        CampoParteProducto.objects.create(
+            nombre=campo, parte_producto=parte, posicion=idx + 1,
+            tipo_de_campo=TipoCampo.objects.get(tipo_interno="ABIERTO"))
+
+def migration():
+    nprods = rn.randint(10, 25)
+    cats = list(CategoriaProducto.objects.all())
+    for np in range(nprods):
+        nombre = f"producto_{np + 1:03}"
+        prod, cprod = Producto.objects.get_or_create(nombre=nombre)
+        if cprod:
+            ncat = rn.randint(0, len(cats))
+            prod.categorias.set(rn.sample(cats, ncat))
+            add_parte(prod)
+
